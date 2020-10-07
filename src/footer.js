f9b92fc5a22f4c913588252cf4996439d55918f2
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-07 16:48:11 +0200

    Add footer

---
+++ src/footer.js
@@ -0,0 +1,29 @@
+const footer = (location, element) => {
+    element.insertAdjacentHTML(location,`<footer>    
+        <section class="width-100 m-0 padding-y-1 centered-h-v black-bg">
+            <div class="centered-h-v margin-y-1">
+                <h6>
+                    <a class="text-1 white-text" href="mailto:emmamugira@gmail.com" target="_blank">emmamugira@gmail.com</a>
+                </h6>
+               </div>
+            <div class="flex-space-between width-40 text-1">
+               
+                <a href="https://github.com/descholar-ceo" target="_blank">
+                    <i class="fa fa-github white-text" aria-hidden="true"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/mugirase-emmanuel" target="_blank">
+                    <i class="fa fa-linkedin white-text" aria-hidden="true"></i>
+                </a>
+                <a href="https://twitter.com/descholar3" target="_blank">
+                    <i class="fa fa-twitter white-text" aria-hidden="true"></i>
+                </a>
+                <a href="https://medium.com/@emmamugira" target="_blank">
+                    <i class="fa fa-medium white-text" aria-hidden="true"></i>
+                </a>
+            </div>
+        </section>
+    </footer>
+`);
+};
+
+export default footer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './assets/styles/styles.scss';
 import NavBar from './NavBar';
 import MainContent from './MainContent';
+import footer from './footer';
 const rootElement = document.querySelector('#content')
 
 NavBar('beforeend', rootElement)
 MainContent('beforeend', rootElement)
+footer('beforeend', rootElement)
