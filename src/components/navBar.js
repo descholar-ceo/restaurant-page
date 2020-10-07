df5df0e7826d36679b6200068503e3c420efdccb
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-07 23:46:06 +0200

    Add sticky navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,17 +1,17 @@
 const navBar = (location, element) => {
   element.insertAdjacentHTML(location, `
-    <header class="primary-bg centered-h-v padding-y-1 padding-lg-half width-100">
-        <nav class="margin-y-1 centered-h-v width-100 flex-md-horizontal-right">
+    <header class="primary-bg centered-h-v width-100">
+        <nav class="centered-h-v width-100 flex-md-horizontal-right">
             <ul class="flex-horizontal-center width-100 width-md-60">
-                <li class="margin-x-1 home" id="nav-item">
+                <li class="padding-1 margin-x-1 active home" id="nav-item">
                     <i class="fa fa-home hidden-sm" aria-hidden="true"></i>
                     Home
                 </li>
-                <li class="margin-x-1 contact" id="nav-item">
+                <li class="padding-1 margin-x-1 contact" id="nav-item">
                     <i class="fa fa-address-card hidden-sm" aria-hidden="true"></i>
                     Contact
                 </li>
-                <li class="margin-x-1 menu" id="nav-item">
+                <li class="padding-1 margin-x-1 menu" id="nav-item">
                     <i class="fa fa-bars hidden-sm" aria-hidden="true"></i>
                     Menu
                 </li>
