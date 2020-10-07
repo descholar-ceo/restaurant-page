a2f834deed2a0c7a77168ecd1d2ae636b4f14a81
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-07 19:31:30 +0200

    Add click event listener to all tabs

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,29 +3,17 @@ const navBar = (location, element) => {
     <header class="primary-bg centered-h-v padding-y-1 padding-lg-half width-100">
         <nav class="margin-y-1 centered-h-v width-100 flex-md-horizontal-right">
             <ul class="flex-horizontal-center width-100 width-md-60">
-                <li class="margin-x-1">
-                    <a class="white-text" href="#home">
-                        <i class="fa fa-home hidden-sm" aria-hidden="true"></i>
-                        <span>Home</span>
-                    </a>
+                <li class="margin-x-1 home" id="nav-item">
+                    <i class="fa fa-home hidden-sm" aria-hidden="true"></i>
+                    <span>Home</span>
                 </li>
-                <li class="margin-x-1">
-                    <a class="white-text" href="#projects">
-                        <i class="fa fa-briefcase hidden-sm" aria-hidden="true"></i>
-                        <span>Projects</span>
-                    </a>
+                <li class="margin-x-1 contact" id="nav-item">
+                    <i class="fa fa-address-card hidden-sm" aria-hidden="true"></i>
+                    <span>Contact</span>
                 </li>
-                <li class="margin-x-1">
-                    <a class="white-text" href="#about-me">
-                        <i class="fa fa-meetup hidden-sm" aria-hidden="true"></i>
-                        <span>About</span>
-                    </a>
-                </li>
-                <li class="margin-x-1">
-                    <a class="white-text" href="#contact">
-                        <i class="fa fa-address-card hidden-sm" aria-hidden="true"></i>
-                        <span>Contact</span>
-                    </a>
+                <li class="margin-x-1 menu" id="nav-item">
+                    <i class="fa fa-bars hidden-sm" aria-hidden="true"></i>
+                    <span>Menu</span>
                 </li>
             </ul>
         </nav>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,36 @@ import './assets/styles/styles.scss';
 import navBar from './components/navBar';
 import menuPage from './components/menu';
 import footer from './components/footer';
+import homePage from './components/home';
+import contactPage from './components/contact';
 
 const rootElement = document.querySelector('#content');
+const mainMenu = () => menuPage('beforeend', rootElement);
+const mainHome = () => homePage('beforeend', rootElement);
+const mainContact = () => contactPage('beforeend', rootElement);
+const mainFooter = () => footer('beforeend', rootElement);
 
 navBar('beforeend', rootElement);
-menuPage('beforeend', rootElement);
-footer('beforeend', rootElement);
+mainMenu();
+
+const allNavItems = document.querySelectorAll('#nav-item');
+
+const tabSwitcher = (moduleToDisplay) => {
+  rootElement.innerHTML = '';
+  moduleToDisplay();
+};
+
+// eventListeners
+allNavItems.forEach(item => {
+  item.addEventListener('click', event => {
+    if (event.target.classList.contains('home')) {
+      tabSwitcher(mainHome);
+    }
+    if (event.target.classList.contains('menu')) {
+      tabSwitcher(mainContact);
+    }
+    if (event.target.classList.contains('contact')) {
+      tabSwitcher(mainFooter);
+    }
+  });
+});
