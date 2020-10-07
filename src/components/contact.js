aa64fd79ae3c79711e2bd1bfab896113c18bb457
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-07 23:26:50 +0200

    Complete the homepage styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,13 +2,13 @@ const contactPage = (location, element) => {
   element.insertAdjacentHTML(location, `
     <section id="contact" class="width-100 padding-2 primary-bg">
       <div class="third-transparent-bg padding-y-1 padding-lg-half width-80 centered-horizontal white-text rounded-corners-1 box-with-shadows">
-      <h1 class="centered-text width-90">We would like to hear from you:</h1>
-      <div class="contact-info margin-y-2 line-height width-90">
+        <h1 class="centered-text width-90">We would like to hear from you:</h1>
+        <div class="contact-info margin-y-2 line-height width-90">
           <h4 class="centered-text centered-text">You can call us on : +250 720 000 000</h4>
           <h4 class="centered-text centered-text">Send us an sms on : +250 720 000 001</h4>
           <h4 class="centered-text centered-text">Or send us an email on : ourrestaurant@restaurant.page</h4>
-      </div>
-      <div class="flex-horizontal-center margin-y-1 width-100">
+        </div>
+        <div class="flex-horizontal-center margin-y-1 width-100">
           <form class="width-90">
               <div class="col-md-6">
                   <div class="margin-y-1 width-100">
@@ -48,7 +48,7 @@ const contactPage = (location, element) => {
                       class="button-white-transparent">Send me message</button>
               </div>
           </form>
-      </div>
+        </div>
       </div>
     </section>
     `);
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,3 @@
-import homePic from '../assets/images/img-0.jpg';
-
 const mainComponent = (location, element) => {
   element.insertAdjacentHTML(location, `
     <main class="main-page width-100 height-100" id="main-container"></main>`);
@@ -7,9 +5,16 @@ const mainComponent = (location, element) => {
 
 const homePage = (location, element) => {
   element.insertAdjacentHTML(location, `
-    <section class="homepage-section width-100">
-      <div>
-        <!--<img src="${homePic}" alt="restaurant page picture"/>-->
+    <section class="homepage-section width-100 padding-2 centered-h-v">
+      <div class="white-transparent-bg padding-5 padding-lg-half width-80 centered-horizontal black-text rounded-corners-1 box-with-shadows">
+        <h1 class="centered-text">Welcome to restaurant page</h1>
+        <p class="centered-text margin-y-2">"
+          With quick-service, fast casual, and meal delivery services,
+          food is getting faster and more convenient by the New York minute. 
+          More and more players seem to be getting into the game.
+          But Panda Express and Panda Express Catering know how to handle fast orders and do it right.
+        "</p>
+        <h1 class="centered-text">Click the menu option above to be able to read all the vailable menu</h1>
       </div>
     </section>
   `);
